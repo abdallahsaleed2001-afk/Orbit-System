ae6f267c5efa49c7d3d7601a5403997b6f3dab5e
abdallahsaleed2001-afk <abdallahsaleed2001@gmail.com>
2026-08-22 15:58:07 +0300

    Fix security command to use new dashboard handlers

--- src/commands/Core/security.js
+++ src/commands/Core/security.js
@@ -1,6 +1,5 @@
 import { SlashCommandBuilder, PermissionFlagsBits, MessageFlags } from 'discord.js';
-import { getSecurityConfig } from '../../services/security/securityService.js';
-import { buildSecurityDashboard } from '../../handlers/securityHandlers.js';
+import { buildSecurityDashboard } from '../../handlers/securityDashboardHandlers.js';
 import { InteractionHelper } from '../../utils/interactionHelper.js';
 
 export default {
@@ -12,27 +11,18 @@ export default {
   category: 'Core',
   async execute(interaction, config, client) {
     if (!interaction.memberPermissions?.has(PermissionFlagsBits.ManageGuild)) {
-      await interaction.reply({ content: 'You need the Manage Server permission to use this panel.', flags: MessageFlags.Ephemeral });
+      await interaction.reply({
+        content: 'You need the Manage Server permission to use this panel.',
+        flags: MessageFlags.Ephemeral,
+      });
       return;
     }
 
     const deferred = await InteractionHelper.safeDefer(interaction, { flags: MessageFlags.Ephemeral });
     if (!deferred) return;
 
-    const securityConfig = await getSecurityConfig(client, interaction.guildId);
-    const view = await buildSecurityDashboard(client, interaction.guild);
-    view.components = view.components.map((row) => {
-      const json = row.toJSON();
-      json.components = json.components.map((component) => ({
-        ...component,
-        custom_id: component.custom_id.replace(':pending', `:${interaction.user.id}`),
-      }));
-      return json;
-    });
+    const view = await buildSecurityDashboard(client, interaction.guild, interaction.user.id);
 
-    await InteractionHelper.safeEditReply(interaction, {
-      embeds: [view.embeds[0]],
-      components: view.components,
-    });
+    await InteractionHelper.safeEditReply(interaction, view);
   },
 };
