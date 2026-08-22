df60ab14372752cfc6e8f7197e856c5318eed9fb
abdallahsaleed2001-afk <abdallahsaleed2001@gmail.com>
2026-08-22 17:47:14 +0300

    fix security button handler loading

--- src/interactions/buttons/security/security.js
+++ src/interactions/buttons/security/security.js
@@ -1,11 +1,9 @@
-import { securityDashboardButtonHandlers } from '../../../handlers/securityDashboardHandlers.js';
-import securityPanelOverrides from '../../../handlers/securityPanelOverrides.js';
+import { securityDashboardButtonHandlers } from '../../../handlers/securityDashboardCore.js';
 import securityDashboardRuleHandlers from '../../../handlers/securityDashboardRuleHandlers.js';
 import securityAutoModDashboard from '../../../handlers/securityAutoModDashboard.js';
 
 export default [
   ...securityDashboardButtonHandlers,
   ...securityDashboardRuleHandlers,
-  ...securityPanelOverrides,
   ...securityAutoModDashboard,
 ];
