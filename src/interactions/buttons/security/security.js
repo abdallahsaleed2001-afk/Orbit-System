91d346acaaa932451f25a5aeae89a22363aed0fc
abdallahsaleed2001-afk <abdallahsaleed2001@gmail.com>
2026-08-22 12:28:00 +0300

    Wire hierarchical security dashboard handlers

--- src/interactions/buttons/security/security.js
+++ src/interactions/buttons/security/security.js
@@ -1,2 +1,6 @@
 import { securityButtonHandlers } from '../../../handlers/securityHandlers.js';
-export default securityButtonHandlers;
+import { securityAdvancedButtonHandlers } from '../../../handlers/securityAdvancedHandlers.js';
+
+// Advanced handlers are last so they intentionally override the older flat
+// AutoMod/Strike handlers while preserving every other Security control.
+export default [...securityButtonHandlers, ...securityAdvancedButtonHandlers];
