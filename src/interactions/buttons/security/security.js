1542c71c3176899fb50683c0ec2826e00a938034
abdallahsaleed2001-afk <abdallahsaleed2001@gmail.com>
2026-08-22 14:54:03 +0300

    Finish Security rule navigation routing

--- src/interactions/buttons/security/security.js
+++ src/interactions/buttons/security/security.js
@@ -2,10 +2,12 @@ import { securityButtonHandlers } from '../../../handlers/securityHandlers.js';
 import { securityAdvancedButtonHandlers } from '../../../handlers/securityAdvancedHandlers.js';
 import { securityDashboardButtonHandlers } from '../../../handlers/securityDashboardHandlers.js';
 import securityPanelOverrides from '../../../handlers/securityPanelOverrides.js';
+import securityDashboardRuleHandlers from '../../../handlers/securityDashboardRuleHandlers.js';
 
 export default [
   ...securityButtonHandlers,
   ...securityAdvancedButtonHandlers,
   ...securityDashboardButtonHandlers,
+  ...securityDashboardRuleHandlers,
   ...securityPanelOverrides,
 ];
