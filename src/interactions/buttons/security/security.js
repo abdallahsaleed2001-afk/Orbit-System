49a7c8801a070cbeb460dda2b279454bd14f4a45
abdallahsaleed2001-afk <abdallahsaleed2001@gmail.com>
2026-08-22 17:52:55 +0300

    Prioritize Security dashboard overrides for AutoMod and Back

--- src/interactions/buttons/security/security.js
+++ src/interactions/buttons/security/security.js
@@ -1,8 +1,10 @@
+import securityDashboardOverrides from '../../../handlers/securityDashboardOverrides.js';
 import { securityDashboardButtonHandlers } from '../../../handlers/securityDashboardCore.js';
 import securityDashboardRuleHandlers from '../../../handlers/securityDashboardRuleHandlers.js';
 import securityAutoModDashboard from '../../../handlers/securityAutoModDashboard.js';
 
 export default [
+  ...securityDashboardOverrides,
   ...securityDashboardButtonHandlers,
   ...securityDashboardRuleHandlers,
   ...securityAutoModDashboard,
