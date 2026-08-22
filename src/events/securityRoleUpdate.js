6a57f345b44b5217c58b395f885a3cca9946505b
abdallahsaleed2001-afk <abdallahsaleed2001@gmail.com>
2026-08-22 10:42:40 +0300

    Fix dangerous permission detection for role escalation

--- src/events/securityRoleUpdate.js
+++ src/events/securityRoleUpdate.js
@@ -1,31 +1,25 @@
-import { Events } from 'discord.js';
+import { Events, PermissionFlagsBits } from 'discord.js';
 import { handleAntiNuke } from '../services/security/antiNuke.js';
+
+const DANGEROUS_PERMISSIONS = [
+  PermissionFlagsBits.Administrator,
+  PermissionFlagsBits.ManageGuild,
+  PermissionFlagsBits.ManageChannels,
+  PermissionFlagsBits.ManageRoles,
+  PermissionFlagsBits.BanMembers,
+  PermissionFlagsBits.KickMembers,
+  PermissionFlagsBits.ManageWebhooks,
+];
+
+function hasDangerousPermission(role) {
+  return DANGEROUS_PERMISSIONS.some(permission => role.permissions.has(permission));
+}
 
 export default {
   name: Events.RoleUpdate,
   async execute(oldRole, newRole) {
     if (!newRole?.guild) return;
-
-    const dangerousBefore = oldRole.permissions.has([
-      'Administrator',
-      'ManageGuild',
-      'ManageChannels',
-      'ManageRoles',
-      'BanMembers',
-      'KickMembers',
-      'ManageWebhooks',
-    ]);
-    const dangerousAfter = newRole.permissions.has([
-      'Administrator',
-      'ManageGuild',
-      'ManageChannels',
-      'ManageRoles',
-      'BanMembers',
-      'KickMembers',
-      'ManageWebhooks',
-    ]);
-
-    if (dangerousAfter && !dangerousBefore) {
+    if (!hasDangerousPermission(oldRole) && hasDangerousPermission(newRole)) {
       await handleAntiNuke(newRole.guild, 'roleUpdate', newRole.id);
     }
   },
