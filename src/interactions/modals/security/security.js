953fe09b22a0fc4173b9f292f0f3f5a81adf5c08
abdallahsaleed2001-afk <abdallahsaleed2001@gmail.com>
2026-08-22 14:53:17 +0300

    Enable hierarchical Security modal controls

--- src/interactions/modals/security/security.js
+++ src/interactions/modals/security/security.js
@@ -1,4 +1,9 @@
 import { securityModalHandlers } from '../../../handlers/securityHandlers.js';
 import { securityAdvancedModalHandlers } from '../../../handlers/securityAdvancedHandlers.js';
+import { securityDashboardModalHandlers } from '../../../handlers/securityDashboardHandlers.js';
 
-export default [...securityModalHandlers, ...securityAdvancedModalHandlers];
+export default [
+  ...securityModalHandlers,
+  ...securityAdvancedModalHandlers,
+  ...securityDashboardModalHandlers,
+];
