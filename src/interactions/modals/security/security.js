856e069a5beb73adae7a66addafa89502d4ea68a
abdallahsaleed2001-afk <abdallahsaleed2001@gmail.com>
2026-08-23 00:45:46 +0300

    fix: register security whitelist modal fixes

--- src/interactions/modals/security/security.js
+++ src/interactions/modals/security/security.js
@@ -1,9 +1,14 @@
 import { securityModalHandlers } from '../../../handlers/securityHandlers.js';
 import { securityAdvancedModalHandlers } from '../../../handlers/securityAdvancedHandlers.js';
 import { securityDashboardModalHandlers } from '../../../handlers/securityDashboardHandlers.js';
+import securityDashboardFixes from '../../../handlers/securityDashboardFixes.js';
+
+const ok = i => i.customId.split(':').at(-1) === i.user.id;
+const deny = i => i.reply({ content: 'This security dashboard belongs to another moderator.', ephemeral: true });
 
 export default [
   ...securityModalHandlers,
   ...securityAdvancedModalHandlers,
   ...securityDashboardModalHandlers,
+  ...securityDashboardFixes.filter(h => h.name.endsWith('_modal')),
 ];
