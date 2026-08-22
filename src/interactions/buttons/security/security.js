ceb4123eca4733efc2e0405e3473de18c1694271
abdallahsaleed2001-afk <abdallahsaleed2001@gmail.com>
2026-08-22 15:58:47 +0300

    Unify security button handlers

--- src/interactions/buttons/security/security.js
+++ src/interactions/buttons/security/security.js
@@ -1,13 +1,9 @@
-import { securityButtonHandlers } from '../../../handlers/securityHandlers.js';
-import { securityAdvancedButtonHandlers } from '../../../handlers/securityAdvancedHandlers.js';
 import { securityDashboardButtonHandlers } from '../../../handlers/securityDashboardHandlers.js';
 import securityPanelOverrides from '../../../handlers/securityPanelOverrides.js';
 import securityDashboardRuleHandlers from '../../../handlers/securityDashboardRuleHandlers.js';
 import securityAutoModDashboard from '../../../handlers/securityAutoModDashboard.js';
 
 export default [
-  ...securityButtonHandlers,
-  ...securityAdvancedButtonHandlers,
   ...securityDashboardButtonHandlers,
   ...securityDashboardRuleHandlers,
   ...securityPanelOverrides,
