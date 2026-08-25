4141d4f29e3c81ba76bc6d991d24a4a07d3954d2
abdallahsaleed2001-afk <abdallahsaleed2001@gmail.com>
2026-08-25 23:19:19 +0300

    fix security role create event export

--- src/events/securityRoleCreate.js
+++ src/events/securityRoleCreate.js
@@ -2,8 +2,8 @@ import { Events } from 'discord.js';
 import { handleAntiNuke } from '../services/security/antiNuke.js';
 
 export default {
-  name: Events.RoleCreate,
-  async execute(role) {
-    if (role.guild) await handleAntiNuke(role.guild, 'roleCreate', role.id);
+  name: Events.GuildRoleCreate,
+  execute(role) {
+    if (role?.guild) return handleAntiNuke(role.guild, 'roleCreate', role.id);
   },
 };
