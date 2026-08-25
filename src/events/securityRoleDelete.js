33f95fbd5a679b05a6db23fe8137d54cf4dcc5e6
abdallahsaleed2001-afk <abdallahsaleed2001@gmail.com>
2026-08-25 23:30:08 +0300

    fix GuildRoleDelete event name

--- src/events/securityRoleDelete.js
+++ src/events/securityRoleDelete.js
@@ -2,8 +2,8 @@ import { Events } from 'discord.js';
 import { handleAntiNuke } from '../services/security/antiNuke.js';
 
 export default {
-  name: Events.RoleDelete,
+  name: Events.GuildRoleDelete,
   async execute(role) {
-    if (role.guild) await handleAntiNuke(role.guild, 'roleDelete', role.id);
+    if (role?.guild) await handleAntiNuke(role.guild, 'roleDelete', role.id);
   },
 };
