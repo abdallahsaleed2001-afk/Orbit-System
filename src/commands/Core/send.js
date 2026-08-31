a3922ed5ba2a21ea8821e1c4f066cf537569fcfb
abdallahsaleed2001-afk <abdallahsaleed2001@gmail.com>
2026-08-31 18:51:56 +0300

    Add /send image command

---
+++ src/commands/Core/send.js
@@ -0,0 +1,91 @@
+import { SlashCommandBuilder, PermissionFlagsBits, AttachmentBuilder, MessageFlags } from 'discord.js';
+import { InteractionHelper } from '../../utils/interactionHelper.js';
+import { replyUserError, ErrorTypes } from '../../utils/errorHandler.js';
+import { successEmbed } from '../../utils/embeds.js';
+
+export default {
+    data: new SlashCommandBuilder()
+        .setName('send')
+        .setDescription('Send an image as the bot.')
+        .setDMPermission(false)
+        .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages)
+        .addSubcommand((subcommand) =>
+            subcommand
+                .setName('image')
+                .setDescription('Send an image from a URL or from your device.')
+                .addStringOption((option) =>
+                    option
+                        .setName('url')
+                        .setDescription('Direct URL of the image.')
+                        .setRequired(false),
+                )
+                .addAttachmentOption((option) =>
+                    option
+                        .setName('file')
+                        .setDescription('Upload an image from your device.')
+                        .setRequired(false),
+                ),
+        ),
+    category: 'Core',
+
+    async execute(interaction) {
+        const url = interaction.options.getString('url');
+        const file = interaction.options.getAttachment('file');
+
+        if (!url && !file) {
+            return replyUserError(interaction, {
+                type: ErrorTypes.VALIDATION,
+                message: 'You must provide either an image URL or upload an image from your device.',
+            });
+        }
+
+        if (url && file) {
+            return replyUserError(interaction, {
+                type: ErrorTypes.VALIDATION,
+                message: 'Use either the image URL or the uploaded file, not both.',
+            });
+        }
+
+        if (url) {
+            try {
+                const parsed = new URL(url);
+                if (!['http:', 'https:'].includes(parsed.protocol)) throw new Error('invalid protocol');
+            } catch {
+                return replyUserError(interaction, {
+                    type: ErrorTypes.VALIDATION,
+                    message: 'Please provide a valid HTTP/HTTPS image URL.',
+                });
+            }
+        }
+
+        if (file && file.contentType && !file.contentType.startsWith('image/')) {
+            return replyUserError(interaction, {
+                type: ErrorTypes.VALIDATION,
+                message: 'The uploaded file must be an image.',
+            });
+        }
+
+        const channel = interaction.channel;
+        const botPermissions = channel?.permissionsFor(interaction.guild.members.me);
+        if (!channel?.isTextBased() || !botPermissions?.has(PermissionFlagsBits.SendMessages)) {
+            return replyUserError(interaction, {
+                type: ErrorTypes.PERMISSION,
+                message: 'I do not have permission to send images in this channel.',
+            });
+        }
+
+        const imageSource = file?.url || url;
+        const fileName = file?.name || 'image';
+
+        const deferred = await InteractionHelper.safeDefer(interaction, { flags: MessageFlags.Ephemeral });
+        if (!deferred) return;
+
+        const sentMessage = await channel.send({
+            files: [new AttachmentBuilder(imageSource, { name: fileName })],
+        });
+
+        return InteractionHelper.safeEditReply(interaction, {
+            embeds: [successEmbed('Image Sent', `The image was sent successfully. [Jump to message](${sentMessage.url})`)],
+        });
+    },
+};
