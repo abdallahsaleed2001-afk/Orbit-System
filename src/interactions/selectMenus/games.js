6a50a8a55bcfd313da88b7a863773df60c081ca5
abdallahsaleed2001-afk <abdallahsaleed2001@gmail.com>
2026-08-31 04:06:48 +0300

    Update GAMES_ROLE_ID to new value

--- src/interactions/selectMenus/games.js
+++ src/interactions/selectMenus/games.js
@@ -1,6 +1,6 @@
 import { ModalBuilder, TextInputBuilder, TextInputStyle, ActionRowBuilder } from 'discord.js';
 
-const GAMES_ROLE_ID = '1543013490313400340';
+const GAMES_ROLE_ID = '1543774154279354398';
 
 function hasGamesRole(interaction) {
   return interaction.member?.roles?.cache?.has(GAMES_ROLE_ID)
